0cfbf2db8d01945579143ffc3615b3b97bdd0af3
Jamie D & Amanuel <pair+seajamied.yihdego@devbootcamp.com>
2017-10-18 19:37:44 -0700

    Add table to display inventory and basic CSS to begin framing view

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,12 +1,7 @@
 class WarehousesController < ApplicationController
 
   def index
-    @inventory = []
-    Warehouse.all.each do |warehouse|
-      warehouse.parts.each do |part|
-        @inventory << part
-      end
-    end
-    @inventory
+    @inventory = Part.all.uniq {|part| part.number }
   end
+
 end
