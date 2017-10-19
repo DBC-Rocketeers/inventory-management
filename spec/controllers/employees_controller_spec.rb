8ed9a0eb8ed2298721d64c1a7bc2b6312f7c30fe
Jamie D & Amanuel <pair+seajamied.yihdego@devbootcamp.com>
2017-10-19 11:42:14 -0700

    tested creating employee and creating sessions

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe EmployeesController, type: :controller do
+  before(:each) {
+    Employee.create(name: 'Admin', admin: true, employee_id: 1, password: 'password')
+    session[:employee_id] = 1
+  }
 
+  describe "GET #new" do
+    it "responds with a status code of 200" do
+      get :new
+      expect(response).to have_http_status 200
+    end
+
+    it "creates an instance of employee" do
+      get :new
+      expect(assigns(:employee)).to be_a Employee
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET #create" do
+    it "responds with a status code of 200" do
+      post :create, { params: { employee: { name: 'Admin', admin: true, employee_id: 1, password: 'password' } } }
+      expect(response).to have_http_status 200
+    end
+
+    it "creates an instance of employee" do
+      post :create, { params: { employee: { name: 'Admin', admin: true, employee_id: 1, password: 'password' } } }
+      expect(assigns(:employee)).to be_a Employee
+    end
+
+    it "renders the create template" do
+      post :create, { params: { employee: { name: 'Admin', admin: true, employee_id: 1, password: 'password' } } }
+      expect(response).to render_template(:create)
+    end
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  before(:each) {
+    Employee.create(name: 'Admin', admin: true, employee_id: 1, password: 'password')
+    session[:employee_id] = 1
+  }
 
+  describe "GET #new" do
+    it "responds with a status code of 200" do
+      get :new
+      expect(response).to have_http_status 200
+    end
+
+    it "renders the index template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET #create" do
+    it "responds with a status code of 302" do
+      post :create, { params: { employee: {admin: true, employee_id: 1, password: 'password' } } }
+      expect(response).to have_http_status 302
+    end
+
+    it "renders the create template" do
+      post :create, { params: { employee: {admin: true, employee_id: 1, password: 'password' } } }
+      expect(response).should redirect_to("/")
+    end
+  end
 end
