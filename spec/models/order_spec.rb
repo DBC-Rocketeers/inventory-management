5d786d6632ec591e2799c7e0265f4b50a42a69ee
yihdego <yihdego@msn.com>
2017-10-20 01:01:17 -0700

    100% test coverage for orders model and controller up to update

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   let!(:warehouse) { Warehouse.create!(name: "Seattle") }
+  let!(:part) { Part.create!(name: "Flux capacitor", number: 2345) }
   let!(:order) { Order.create!(warehouse_id: 1) }
   describe 'methods' do
     it "creates a new entry" do
       expect{ order.add_entry("Nose Cone", 1234, 2) }.to change{ order.entries.count }.by 1
+    end
+
+    it "creates a new entry when a part already exists" do
+      expect{ order.add_entry("Flux capacitor", 2345, 1) }.to change{ order.entries.count }.by 1
     end
   end
 end
