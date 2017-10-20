d64ea49e69447e00f3e7eea1d204d05823eab638
Piper Denney <denney.piper@gmail.com>
2017-10-19 18:56:41 -0700

    added tests for inventory model method

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -7,20 +7,6 @@ RSpec.describe Part, type: :model do
     let!(:nose2) { Part.create(name: "Nose Cone", number: 1234) }
     let!(:flux) { Part.create(name: "Flux capacitor", number: 2345) }
 
-
-  xdescribe "methods" do
-    it 'it returns the global count of multiple parts' do
-      expect(nose.global_count).to eq 2
-    end
-
-    it 'it returns the global count for a different instances of a part' do
-      expect(nose2.global_count).to eq 2
-    end
-
-    it 'returns the global count of a single instance of a part' do
-      expect(flux.global_count).to eq 1
-    end
-
     it 'it returns the local count of multiple parts' do
       seattle.parts << nose
       seattle.parts << nose2
