74a67541e70c04b6f5b780c6f5ad6c837f9db773
Piper Denney <denney.piper@gmail.com>
2017-10-19 17:03:14 -0700

    form for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def new
     @warehouse = Warehouse.all
+    @inventory = Part.all
   end
 
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,12 +1,4 @@
 class Warehouse < ApplicationRecord
   has_many :inventories
   has_many :parts, through: :inventories
-
-  def location_list
-    names_list = []
-    Warehouse.all.each do |warehouse|
-      names_list << warehouse.name
-    end
-    names_list
-  end
 end
